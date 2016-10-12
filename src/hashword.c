60327c9c7f25dfa669b31c5c9f828a50c2bdb1c0
leminski <ghemon_29@hotmail.it>
2016-10-12 21:30:17 +0200

    Update

--- src/hashword.c
+++ src/hashword.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2016 leminski: Site: https://github.com/leminski
+ *  Copyright (C) 2016 leminski <atleminski@gmail.com> https://github.com/leminski
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
