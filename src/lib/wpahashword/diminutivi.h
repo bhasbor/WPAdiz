a52428c12415275dba272b2fdda58d7f8998fff6
Leminski <atleminski@gmail.com>
2016-10-13 04:20:02 +0200

    Update diminutivi.h

--- src/lib/wpahashword/diminutivi.h
+++ src/lib/wpahashword/diminutivi.h
@@ -1,4 +1,26 @@
 #include <string.h>
 #include <stdio.h>
 
+/* Desinenze diminutivi maschili */
+
+ #define ONE  "one"
+ #define ETTO "etto"
+ #define INO  "ino"
+ #define OTTO "otto"
+
+ /* Desinenze diminutivi femminili */
+
+ #define ONA  "ona"
+ #define ETTA "etta"
+ #define INA  "ina"
+ #define OTTA "otta"
+
+ /* Vocali */
+
+ #define A     'a'
+ #define E     'e'
+ #define I     'i'
+ #define O     'o'
+ #define U     'u'
+
 void conc_diminutivi(FILE*, FILE*);
