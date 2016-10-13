b48bee0350bd63d9fcae49bae994e175de54a7eb
Leminski <atleminski@gmail.com>
2016-10-13 04:17:06 +0200

    Update outerr.c

--- src/lib/outerr.c
+++ src/lib/outerr.c
@@ -26,34 +26,6 @@
 
 #include "outerr.h"
 
- /* Define for programm hashword */
-
- #define INVALID_PARAM        -3  /* Errore di sintassi nell input digitato */
- #define INVALID_ERR_PAR_NUM  -4  /* Errore di sintassi nell input immesso nel primo parametro */
- #define INVALID_ERR_PAR_NUM2 -5  /* Errore di sintassi nell input immesso nel secondo parametro */
- #define SINTAX_MANY_TRATT    -6  /* Se vengono aggiunti due o piu simboli '-' */
- #define ERROR_PARAM_T        -7
- #define ERROR_PARAM_D        -8
- #define ERROR_PARAM_C        -9
- #define ERROR_OUT            -20
-
- /* Define for programm legth */
-
- #define ERROR_PARAM_F       -9
- #define ERROR_PARAM_E       -10
- #define INVALID_PARAM_L     -11
- #define INVALID_NUMBER      -12
-
- /* Define for programm date */
-
- #define ERROR_PARAM       -15
- #define ERROR_OUT_D       -21
- #define INVALID_DAY       -16
- #define INVALID_MONTH     -17
- #define INVALID_YEAR      -18
- #define INVALID_END_YEAR  -19
-
-
 void  /*Programm hashword */
   outerr_hash(char* buffer, char* buffer2, short int _flag_err)
 
